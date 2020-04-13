519b6c3c8972d4fab649c6f084fbe341457ba8eb
Tomasz Kalbarczyk <tomasz@symmetry-systems.com>
2020-04-13 18:20:05 -0500

    changes to env script

--- env.sh
+++ env.sh
@@ -8,7 +8,7 @@ export EDITOR='subl -w'
 
 # Virtual Environment
 export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Dropbox/Development
+export PROJECT_HOME=$HOME/dev
 source /usr/local/bin/virtualenvwrapper.sh
 
 # Owner
